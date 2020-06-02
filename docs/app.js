3f7219edf5528ba2bd2704a9ca77f248f111a30a
Lindsay Johnston <lindsaykjohnston@outlook.com>
2020-06-02 10:41:16 -0700

    Completed addTask() and removeTask() functions

--- docs/app.js
+++ docs/app.js
@@ -1,19 +1,25 @@
 const dateArea= document.querySelector('#date');
 const timeArea=document.querySelector("#time");
-
+const form=document.querySelector("#task-form");
+const input=document.querySelector("#task-input");
+const taskList=document.querySelector(".task-collection");
 
 setEventListeners();
 
 function setEventListeners(){
-    document.addEventListener('DOMContentLoaded', loadDateTime())
+    document.addEventListener('DOMContentLoaded', loadDateTime);
+    form.addEventListener('submit', addNewTask);
+    taskList.addEventListener('click', removeTask);
+
 }
+
+//LOAD DATE AND TIME
 
 function loadDateTime(){
     let date= new Date();
     let hours= date.getHours();
     let minutes= date.getMinutes();
     minutes= checkTime(minutes);
-    console.log(minutes);
     hours= checkTime(hours);
     let time= hours + ":" + minutes;
     date= date.toDateString();
@@ -24,6 +30,7 @@ function loadDateTime(){
     t=setTimeout(function(){
         loadDateTime();
     }, 500);
+    
 }
 
 function checkTime(i){
@@ -33,4 +40,35 @@ function checkTime(i){
     return i;
 }
 
+///ADD NEW TASK
+
+function addNewTask(event){
+    if(input.value===""){
+        alert("Enter a task!");
+    }
+
+    let li= document.createElement('li');
+    li.className='task-item';
+    li.appendChild(document.createTextNode(input.value));
+    let link= document.createElement('a');
+    link.className='delete-item';
+    link.innerHTML='<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+    console.log(li);
+    taskList.appendChild(li);
+    input.value='';
+
+    event.preventDefault();
+   
+}
+
+//REMOVE TASK
+
+function removeTask(event){
+    if(event.target.classList.contains('fa-remove')){
+        if(confirm('Are you sure you want to delete this task?')){
+            event.target.parentElement.parentElement.remove();
+        }
+    }
+}
 
